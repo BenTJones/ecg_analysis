15206e823de488099ba4b95b57469f630b0ca134
Ben Jones <benjones012105@gmail.com>
2026-06-19 22:18:08 +0100

    Updated stuff

--- src/plots.py
+++ src/plots.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from src.data_extraction import load_ecg, plot_ecg
+
 
 def loss_curve(history,save_path = 'results/plots/loss_curve.png'):
     plt.figure(figsize=(9,5))
@@ -22,4 +24,8 @@ def val_metrics(history,save_path = 'results/plots/val_metrics.png'):
     plt.legend()
     plt.savefig(save_path)
     plt.show()
+    
+def plot_ecg_by_id(ecg_id,df,path = 'data/'):
+    signal,meta,row = load_ecg(ecg_id,df,path)
+    plot_ecg(signal,meta,title = f"ECG ID: {ecg_id}")
     
---
+++ src/saliency_map.py
@@ -0,0 +1,5 @@
+from captum.attr import Saliency
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
